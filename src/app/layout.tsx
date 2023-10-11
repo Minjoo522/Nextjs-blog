7145f6964817cb6e948aefebffb03e6644f11910
Kim Minjoo(김민주) <131349867+Minjoo522@users.noreply.github.com>
2023-10-11 19:54:40 +0900

    Header 및 Footer 관련 기능 + 스타일링 (#2)
    * feat : Header 기능 추가
    
    * design : CSS 스타일링
    
    * refactor : Header, Footer Component화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
@@ -12,7 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className={sans.className}>
-      <body>{children}</body>
+      <body className='flex flex-col w-full max-w-screen-2xl mx-auto'>
+        <Header />
+        <main className='grow'>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className='p-2 bg-slate-500 text-white text-sm text-center font-semibold'>
+        {"Welcome to Minjoo's Blog | All Right Reserved."}
+      </p>
+    </footer>
+  );
+}
